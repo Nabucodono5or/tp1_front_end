79b27a82e0778ecd4e360d98fe7cb552adbd1077
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-04-23 17:13:25 -0300

    resolvida questão da directiva de enter

--- categorias/categorias.js
+++ categorias/categorias.js
@@ -1,8 +1,30 @@
 (function () {
   function categoriaController(){
     this.categorias = ['Casa', 'Trabalho', 'Faculdade'];
+
+    this.myFunction = () => {
+      console.log("sucesso");
+    }
   }
 
   categoriaController.$inject = [];
-  angular.module('tp1').controller('categoriaController', categoriaController);
+  angular.module('tp1')
+  .controller('categoriaController', categoriaController)
+  .directive('dlEnterKey', function() {
+    return function(scope, element, attrs) {
+
+        element.bind("keydown keypress", function(event) {
+            var keyCode = event.which || event.keyCode;
+
+            if (keyCode === 13) {
+                scope.$apply(function() {
+                    scope.$eval(attrs.dlEnterKey);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
+
 })()
