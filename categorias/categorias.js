9d4001339d3bde11520efa3c2978f26ded45361e
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-04-23 17:59:34 -0300

    refatorando código de categoria,js

--- categorias/categorias.js
+++ categorias/categorias.js
@@ -1,30 +1,55 @@
-(function () {
-  function categoriaController(){
+(function() {
+  function categoriaController() {
     this.categorias = ['Casa', 'Trabalho', 'Faculdade'];
 
     this.myFunction = () => {
+
       console.log("sucesso");
+
+      /*
+          let e = this.entrada;
+
+          function entradaValida(){
+            for (let i = 0; i < categorias.length; i++) {
+              if(categorias[i] == e){
+                return false;
+              }
+            }
+
+            return true;
+          }
+
+      */
     }
   }
 
   categoriaController.$inject = [];
   angular.module('tp1')
-  .controller('categoriaController', categoriaController)
-  .directive('dlEnterKey', function() {
-    return function(scope, element, attrs) {
+    .controller('categoriaController', categoriaController);
+
+})();
+
+
+(function() {
+  angular.module('tp1')
+    .directive('dlEnterKey', function() {
+      return function(scope, element, attrs) {
 
         element.bind("keydown keypress", function(event) {
-            var keyCode = event.which || event.keyCode;
+          var keyCode = event.which || event.keyCode;
 
-            if (keyCode === 13) {
-                scope.$apply(function() {
-                    scope.$eval(attrs.dlEnterKey);
-                });
+          if (keyCode === 13) {
+            scope.$apply(function() {
+              scope.$eval(attrs.dlEnterKey);
+            });
 
-                event.preventDefault();
-            }
+            event.preventDefault();
+          }
         });
-    };
-});
+      };
+    })
+    .service('entradaValida', function() {
 
-})()
+    });
+
+})();
