6aa0ba3a79d3ecb15853924544293bf4fdfc3a5f
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-04-24 14:14:11 -0300

    criado estado do terceiro cenario e realocação dos services para global

--- app/tp1.js
+++ app/tp1.js
@@ -1,3 +1,50 @@
 (function() {
   angular.module('tp1', []);
 })();
+
+(function() {
+  angular.module('tp1')
+    .directive('dlEnterKey', function() {
+      return function(scope, element, attrs) {
+
+        element.bind("keydown keypress", function(event) {
+          var keyCode = event.which || event.keyCode;
+
+          if (keyCode === 13) {
+            scope.$apply(function() {
+              scope.$eval(attrs.dlEnterKey);
+            });
+
+            event.preventDefault();
+          }
+        });
+      };
+    })
+    .service('entradaValida', function() {
+      this.repetido = function (valor, array) {
+        for (let i = 0; i < array.length; i++) {
+          if(array[i].toLowerCase() === valor.toLowerCase()){
+            return true;
+          }
+        }
+        return false;
+      }
+
+      this.padrao = function (valor) {
+        if(valor.length === 0 || !valor.trim() || eNumero(valor)){
+          return false;
+        }
+        return true;
+      }
+
+      function eNumero(valor) {
+        if(parseInt(valor)){
+          return true;
+        }
+
+        return false;
+      }
+
+    });
+
+})();
---
+++ tarefas/adcionar_tarefas.js
@@ -0,0 +1,95 @@
+(function() {
+  function tarefaController(entradaValida) {
+    this.categorias = ['Casa', 'Trabalho', 'Faculdade'];
+
+    this.tarefas = [{
+        nome: 'Varrer o quarto',
+        categoria: 'Casa',
+        feito: false
+      },
+      {
+        nome: 'Lavar a louça',
+        categoria: 'Casa',
+        feito: true
+      },
+      {
+        nome: 'Lavar as roupas',
+        categoria: 'Casa',
+        feito: false
+      },
+      {
+        nome: 'Consertar a porta',
+        categoria: 'Casa',
+        feito: false
+      },
+      {
+        nome: 'Terminar o relatório',
+        categoria: 'Trabalho',
+        feito: false
+      },
+      {
+        nome: 'Estudar para a próxima reunião',
+        categoria: 'Trabalho',
+        feito: false
+      },
+      {
+        nome: 'Enviar email sobre o problema',
+        categoria: 'Trabalho',
+        feito: false
+      },
+      {
+        nome: 'Levar pó de café',
+        categoria: 'Trabalho',
+        feito: true
+      },
+      {
+        nome: 'Comprar canetas novas',
+        categoria: 'Trabalho',
+        feito: true
+      },
+      {
+        nome: 'Estudar Etapa 1',
+        categoria: 'Faculdade',
+        feito: true
+      },
+      {
+        nome: 'Estudar Etapa 2',
+        categoria: 'Faculdade',
+        feito: true
+      },
+      {
+        nome: 'Estudar Etapa 3',
+        categoria: 'Faculdade',
+        feito: false
+      },
+      {
+        nome: 'Fazer TP1',
+        categoria: 'Faculdade',
+        feito: false
+      }
+    ];
+
+    this.entrada="";
+    this.sucesso=false;
+    this.erro = false;
+
+    this.myFunction = () => {
+
+      if(!entradaValida.padrao(this.entrada) || entradaValida.repetido(this.entrada, this.categorias)){
+        console.log("erro");
+        this.sucesso = false;
+        this.erro = true;
+      } else {
+        this.sucesso = true;
+        this.erro = false;
+        this.categorias.push(this.entrada);
+        console.log("sucesso");
+      }
+    }
+  }
+
+  tarefaController.$inject = ['entradaValida'];
+
+  angular.module('tp1').controller('tarefaController', tarefaController);
+
+})()
